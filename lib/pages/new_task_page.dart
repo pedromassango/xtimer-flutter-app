4ad04b0ff5b77981bd25bbb14006a6415438d2d4
pedromassango <pedromassango.developer@gmail.com>
2018-09-17 11:00:16 +0100

    small updates

--- lib/pages/new_task_page.dart
+++ lib/pages/new_task_page.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
-class NewTaskPage extends StatefulWidget{
-  final String title = 'X - New Task';
+class NewTaskPage extends StatefulWidget {
+  final String title = 'New Task';
+
   @override
   _NewTaskPageState createState() => _NewTaskPageState();
 }
 
-class _NewTaskPageState extends State<NewTaskPage>{
+class _NewTaskPageState extends State<NewTaskPage> {
+
+  /// When called, save task and close this screen
+  void _saveTaskAndClose(){
+
+    // close screen
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,22 +22,24 @@ class _NewTaskPageState extends State<NewTaskPage>{
       appBar: new AppBar(
         elevation: 0.0,
         backgroundColor: Colors.transparent,
-        title:  new Text(widget.title, style:
-        TextStyle(color: Colors.black,
-            fontSize: 32.0,
-            fontWeight: FontWeight.bold),
+        title: new Text(
+          widget.title,
+          style: TextStyle(
+              color: Colors.black, fontSize: 32.0, fontWeight: FontWeight.bold),
         ),
         actions: <Widget>[
           new IconButton(
-            icon: Icon(Icons.close, color: Colors.black, size: 32.0,),
-            onPressed: (){
-              Navigator.of(context).pop();
-            },
+            icon: Icon(
+              Icons.done,
+              color: Colors.black,
+              size: 32.0,
+            ),
+            onPressed: _saveTaskAndClose,
           )
         ],
       ),
       body: Container(
-        margin: EdgeInsets.only(top: 32.0),
+        margin: EdgeInsets.only(top: 50.0),
         child: Column(
           children: <Widget>[
 
