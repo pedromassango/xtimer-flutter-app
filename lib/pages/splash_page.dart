99814a032c490ba603332fa184feee1db0593d60
pedromassango <pedromassango.developer@gmail.com>
2018-09-17 01:37:34 +0100

    Splash almost ready

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class SplashPage extends StatefulWidget {
   SplashPage({Key key}) : super(key: key);
@@ -9,14 +10,32 @@ class SplashPage extends StatefulWidget {
 
 class _SplashPageState extends State<SplashPage> {
 
+  startTimer() async {
+
+    // pause for a while then start home screen
+    var duration = Duration(seconds: 3);
+    return Timer(duration, () {
+      Navigator.pushReplacementNamed(context, '/home');
+    });
+  }
+  
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: new Center(
-        child: Image.asset(
-          'images/logo.png',
-          width: 95.0,
-          height: 95.0,
+        child: Hero(
+          tag: 'logo',
+          child: Image.asset(
+            'images/logo.png',
+            width: 95.0,
+            height: 95.0,
+          ),
         ),
       ),
     );
