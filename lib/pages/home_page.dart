8f06c726f937b9dc711cfec92d153bed2460ecd8
pedromassango <pedromassango.developer@gmail.com>
2018-09-17 10:54:05 +0100

    code reformatted

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:xtimer/widgets/task_widget.dart';
 import 'package:xtimer/pages/timer_page.dart';
+import 'package:xtimer/pages/new_task_page.dart';
+
 import 'package:flutter/services.dart';
 import 'package:xtimer/model/task_model.dart';
 import 'package:flutter/cupertino.dart';
 
 class HomePage extends StatefulWidget {
-
   final String title = 'X - Timer';
 
   @override
@@ -14,7 +15,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   /// A set of tasks
   List<Task> tasksList = [
     Task(Colors.green, 'Study', DateTime.now()),
@@ -28,26 +28,25 @@ class _HomePageState extends State<HomePage> {
   ];
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     // Set this screen as a fullscreen
     SystemChrome.setEnabledSystemUIOverlays([]);
   }
 
-  /// When called, close this Screen
-  void _closePage() {
-    Navigator.of(context).pop();
+  /// When called, start new task
+  void _startNewTaskPage() {
+    Navigator.of(context, rootNavigator: true).push(new CupertinoPageRoute(
+        fullscreenDialog: true, builder: (buildContext) => NewTaskPage()));
   }
 
   /// When called start timer Screen
-  void _startTimerPage(Task task){
-    Navigator
-        .of(context, rootNavigator: true)
-        .push(new CupertinoPageRoute<bool>(
-        fullscreenDialog: true,
-        builder: (buildContext) => TimerPage(task: task)
-    ));
-        //.push(MaterialPageRoute(builder: (context) => TimerPage(task: task,)));
+  void _startTimerPage(Task task) {
+    Navigator.of(context, rootNavigator: true).push(
+        new CupertinoPageRoute<bool>(
+            fullscreenDialog: true,
+            builder: (buildContext) => TimerPage(task: task)));
+    //.push(MaterialPageRoute(builder: (context) => TimerPage(task: task,)));
   }
 
   @override
@@ -56,32 +55,36 @@ class _HomePageState extends State<HomePage> {
       appBar: new AppBar(
         elevation: 0.0,
         backgroundColor: Colors.transparent,
-        title:  new Text(widget.title, style:
-        TextStyle(color: Colors.black,
-              fontSize: 32.0,
-              fontWeight: FontWeight.bold),
-          ),
+        title: new Text(
+          widget.title,
+          style: TextStyle(
+              color: Colors.black, fontSize: 32.0, fontWeight: FontWeight.bold),
+        ),
         //TODO: leading: Icon(Icons.menu, color: Colors.black,),
         actions: <Widget>[
           new IconButton(
-            icon: Icon(Icons.add, color: Colors.black, size: 32.0,),
-            onPressed: _closePage,
+            icon: Icon(
+              Icons.add,
+              color: Colors.black,
+              size: 32.0,
+            ),
+            onPressed: _startNewTaskPage,
           )
         ],
       ),
       body: Container(
         margin: EdgeInsets.only(top: 32.0),
         child: ListView.builder(
-              itemCount: tasksList.length,
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemBuilder: (BuildContext context, int index){
-                var item = tasksList.elementAt(index);
-                return GestureDetector(
-                  child: TaskWidget(task: item),
-                  onTap: ()=> _startTimerPage(item),
-                );
-              },
+          itemCount: tasksList.length,
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, int index) {
+            var item = tasksList.elementAt(index);
+            return GestureDetector(
+              child: TaskWidget(task: item),
+              onTap: () => _startTimerPage(item),
+            );
+          },
         ),
       ),
     );
