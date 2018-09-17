6ad190cb2e32760046517c4b3472e1c03944406a
pedromassango <pedromassango.developer@gmail.com>
2018-09-17 02:53:55 +0100

    from Stateless to Stateful widget

--- lib/widgets/rounded_button_widget.dart
+++ lib/widgets/rounded_button_widget.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class RoundedButton extends StatelessWidget{
+class RoundedButton extends StatefulWidget{
   final String text;
   RoundedButton({Key key, @required this.text}): super(key: key);
+
+  @override
+  _RoundedButtonState createState() => _RoundedButtonState();
+}
+
+class _RoundedButtonState extends State<RoundedButton>{
 
   @override
   Widget build(BuildContext context) {
@@ -10,21 +16,21 @@ class RoundedButton extends StatelessWidget{
       width: 160.0,
       height: 160.0,
       decoration: new BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(100.0),
-        boxShadow: <BoxShadow>[
-          BoxShadow(
-            color: Colors.white,
-            blurRadius: 0.0
-          )
-        ]
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(100.0),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.white,
+                blurRadius: 0.0
+            )
+          ]
       ),
       child: Center(
-        child: Text(text.toUpperCase(),
-        style: TextStyle(
-          fontSize: 24.0,
-          fontWeight: FontWeight.bold
-        ),
+        child: Text(widget.text.toUpperCase(),
+          style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold
+          ),
         ),
       ),
     );
