18d02c849086227189f00f070d3870a5ed453b9f
pedromassango <pedromassango.developer@gmail.com>
2018-09-17 10:25:35 +0100

    removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SplashPage(),
       routes: <String, WidgetBuilder>{
         '/home': (BuildContext context) => HomePage(),
