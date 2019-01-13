f98720c889ba009deb5f453b955d8615887b3759
pedromassango <pedromassango.developer@gmail.com>
2019-01-14 00:04:07 +0100

    small updates to sqflite support

--- lib/model/task_model.dart
+++ lib/model/task_model.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 class Task{
+  final int id;
   final Color color;
   final String title;
   final int minutes;
   
-  Task(this.color, this.title, this.minutes);
-  
+  Task(this.id, this.color, this.title, this.minutes);
+
+  factory Task.fromMap(Map<String, dynamic> json) => Task(
+      json['id'],
+      Color(json['color']),
+      json['title'],
+      json['minutes']
+  );
 }
