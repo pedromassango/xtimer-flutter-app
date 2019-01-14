c876dac39b5c1b5f4c4654ac23e3738f513ae265
pedromassango <pedromassango.developer@gmail.com>
2019-01-14 01:27:06 +0100

    now saving tasks in database

--- lib/controllers/task_manager.dart
+++ lib/controllers/task_manager.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
-
+import 'package:sqflite/sql.dart';
+import 'package:xtimer/controllers/Database.dart';
 import 'package:xtimer/model/task_model.dart';
 
 class TaskManager {
-  static DateTime now() => DateTime.now();
+
+  DatabaseProvider dbProvider;
 
   /// A set of tasks
   static List<Task> tasksList = [
-    Task(Colors.purple, 'Pratice Flutter', 1),
-    Task(Colors.amber, 'Read about Bitcoin', 3),
-    Task(Colors.deepOrange, 'Learn English', 2),
-    Task(Colors.teal, 'Meditation', 5),
+    Task(1, Colors.purple, 'Pratice Flutter', 1),
+    Task(2, Colors.amber, 'Read about Bitcoin', 3),
+    Task(3, Colors.deepOrange, 'Learn English', 2),
   ];
 
-  static addNewTask(Task task) => tasksList.add(task);
+  addNewTask(Task task) {
+    dbProvider = DatabaseProvider.db;
+    dbProvider.insert(task);
+  }
+
+  Future<List<Task>> getAll() async {
+    dbProvider = DatabaseProvider.db;
+    return dbProvider.getAll();
+  }
 }
