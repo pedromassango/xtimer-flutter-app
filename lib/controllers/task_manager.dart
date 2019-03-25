b157a94a305a16c75b5ccaffbade643ac97971ed
pedromassango <pedromassango.developer@gmail.com>
2019-03-25 22:22:35 +0100

    database manager improved

--- lib/controllers/task_manager.dart
+++ lib/controllers/task_manager.dart
@@ -1,20 +1,23 @@
+import 'dart:async';
+
 import 'package:xtimer/controllers/Database.dart';
 import 'package:xtimer/model/task_model.dart';
 
 class TaskManager {
 
-  DatabaseProvider _dbProvider;
+  final DatabaseProvider dbProvider;
+
+  TaskManager({ this.dbProvider});
+
   Future<List<Task>> _tasksData;
 
   Future<List<Task>> get tasksData => _tasksData;
 
-  addNewTask(Task task) {
-    _dbProvider = DatabaseProvider.db;
-    _dbProvider.insert(task);
+  Future<void> addNewTask(Task task) async {
+    dbProvider.insert(task);
   }
 
-  loadAllTasks(){
-    _dbProvider = DatabaseProvider.db;
-    _tasksData = _dbProvider.getAll();
+  Future<void> loadAllTasks() async {
+    _tasksData = dbProvider.getAll();
   }
 }
