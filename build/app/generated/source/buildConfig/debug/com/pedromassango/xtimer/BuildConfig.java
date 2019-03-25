d7d3081902e3b5bf9581d553a1b361036e65fa1e
pedromassango <pedromassango.developer@gmail.com>
2019-03-25 22:06:20 +0100

    v4

--- build/app/generated/source/buildConfig/debug/com/pedromassango/xtimer/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/com/pedromassango/xtimer/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.pedromassango.xtimer";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 3;
-  public static final String VERSION_NAME = "2.3.0";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "2.4.0";
 }
