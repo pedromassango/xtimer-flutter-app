f0081d497b9d8ea46c2f9107996b214fcdcf91b6
pedromassango <pedromassango.developer@gmail.com>
2019-03-25 23:04:59 +0100

    initialized home bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:xtimer/controllers/Database.dart';
+import 'package:xtimer/controllers/task_manager.dart';
+import 'package:xtimer/pages/home_page/home_bloc.dart';
 import 'package:xtimer/pages/new_task_page.dart';
 import 'package:xtimer/pages/splash_page.dart';
 import 'package:xtimer/pages/home_page/home_page.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(new MyApp());
+class MyDelegate extends BlocDelegate{
+  @override
+  void onTransition(Transition transition) {
+    print(transition);
+  }
+}
 
-class MyApp extends StatelessWidget {
+void main(){
+  BlocSupervisor().delegate = MyDelegate();
+
+  DatabaseProvider dbProvider = DatabaseProvider.db;
+  TaskManager taskManager = TaskManager(dbProvider: dbProvider);
+  HomeBloc homeBloc = HomeBloc(taskManager: taskManager);
+
+  runApp(new MyApp(
+    homeBloc: homeBloc,
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  final HomeBloc homeBloc;
+  const MyApp({Key key, this.homeBloc}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void dispose() {
+    widget.homeBloc.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashPage(),
-      routes: <String, WidgetBuilder>{
-        '/home': (BuildContext context) => HomePage(),
-        '/new': (context) => NewTaskPage(),
-      },
+    return BlocProvider<HomeBloc>(
+      bloc: widget.homeBloc,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: SplashPage(),
+        routes: <String, WidgetBuilder>{
+          '/home': (BuildContext context) => HomePage(),
+          '/new': (context) => NewTaskPage(),
+        },
+      ),
     );
   }
 }
